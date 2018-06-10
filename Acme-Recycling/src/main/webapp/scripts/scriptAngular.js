dad9f67b9b58c4b2bc66b1829e5772f747d7ddcc
espisepi <sepinaco@gmail.com>
2018-06-11 01:26:41 +0200

    feat: consumiendo de nuestra api en scriptAngular.js

--- Acme-Recycling/src/main/webapp/scripts/scriptAngular.js
+++ Acme-Recycling/src/main/webapp/scripts/scriptAngular.js
@@ -1,10 +1,19 @@
 var app = angular.module("app", []);
 
-app.controller("PruebaController", function($scope) {
-	$scope.opinion = {
-		title : "hola",
-		comment : "",
-		photo : "",
-		caption : ""
-	}
-});
+// Todo lo que añadamos como parametro en nuestra function del controlador se instancian ya que son singleton
+// app.controller("PruebaController", function($scope,$log, $http) {
+app.controller("PruebaController", [
+		'$scope', '$log', '$http', function($scope, $log, $http) {
+			$http.get("http://localhost:8080/Acme-Recycling/materialJSON/listJSON.do").success(function(data, status, headers, config) {
+
+				// $log.debug() es similar a poner console.log()
+				$log.debug(data);
+				$log.debug("status: " + status);
+				$log.debug("headers: " + headers);
+				$log.debug("config: " + config);
+
+			}).error(function(err, status, headers, config) {
+				$log.debug("Ha fallado la petición. Estado HTTP:" + status);
+			});
+		}
+]);
