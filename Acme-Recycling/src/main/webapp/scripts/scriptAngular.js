cb862819ba41dd5b906ddb91dd14e44b9adbeb8e
espisepi <sepinaco@gmail.com>
2018-06-11 04:36:55 +0200

    feat: mostrando materials en el html consumiendo la api

--- Acme-Recycling/src/main/webapp/scripts/scriptAngular.js
+++ Acme-Recycling/src/main/webapp/scripts/scriptAngular.js
@@ -4,13 +4,17 @@ var app = angular.module("app", []);
 // app.controller("PruebaController", function($scope,$log, $http) {
 app.controller("PruebaController", [
 		'$scope', '$log', '$http', function($scope, $log, $http) {
+			// Creamos la variable que guardaremos nuestro JSON
+			$scope.materiales = [];
+
+			// Obtenemos el Json por medio del singletone $http
 			$http.get("http://localhost:8080/Acme-Recycling/materialJSON/listJSON.do").success(function(data, status, headers, config) {
 
 				// $log.debug() es similar a poner console.log()
 				$log.debug(data);
-				$log.debug("status: " + status);
-				$log.debug("headers: " + headers);
-				$log.debug("config: " + config);
+
+				// Guardamos en nuestro array materiales el json obtenido
+				$scope.materiales = data;
 
 			}).error(function(err, status, headers, config) {
 				$log.debug("Ha fallado la petición. Estado HTTP:" + status);
